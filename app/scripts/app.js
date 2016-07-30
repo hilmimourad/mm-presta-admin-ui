6547428260f1df6e57c9deee1f8277ca4075ae0b
Mourad Hilmi <mourad.hilmi2@gmail.com>
2016-07-30 21:33:36 +0100

    ok

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mmPrestaAdminUiApp
+ * @description
+ * # mmPrestaAdminUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mmPrestaAdminUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
